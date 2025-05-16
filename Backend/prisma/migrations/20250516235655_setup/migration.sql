c0f009cd40648013b250de6164f8baf279609764
a <abdulrhmanabotor@gmail.com>
2025-05-17 02:59:21 +0300

    docs(Backend): add local run steps and fix schema.prisma syntax
    Add a new markdown file with steps to run the backend locally. Fix a syntax error in the schema.prisma file by closing a missing quotation mark.

---
+++ Backend/prisma/migrations/20250516235655_setup/migration.sql
@@ -0,0 +1,47 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `Category` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Booking" DROP CONSTRAINT "Booking_eventId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Category" DROP CONSTRAINT "Category_parentCategoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Event" DROP CONSTRAINT "Event_categoryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "EventTag" DROP CONSTRAINT "EventTag_eventId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "EventTag" DROP CONSTRAINT "EventTag_tagId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "EventTranslation" DROP CONSTRAINT "EventTranslation_eventId_fkey";
+
+-- AlterTable
+ALTER TABLE "Booking" ALTER COLUMN "eventId" DROP NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Event" ADD CONSTRAINT "Event_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EventTranslation" ADD CONSTRAINT "EventTranslation_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_parentCategoryId_fkey" FOREIGN KEY ("parentCategoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EventTag" ADD CONSTRAINT "EventTag_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "EventTag" ADD CONSTRAINT "EventTag_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
